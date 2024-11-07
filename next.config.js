c9cae174c2e419ceb0614608f1eeb483f5ef7427
Michael Manzano <cm172596@gmail.com>
2024-11-07 01:47:30 -0300

    update snap name

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
         ETHERSCAN_API_KEY: "CJAVH2PWQT6IXZ1T6GN2S77MRIM1RJJSZ9",
         GITCOIN_API_KEY: "LvYOPkBN.En8SN25VOyLOfDrpWo3xZkjgrrhVAIvZ",
         GITCOIN_SCORER_ID: "8195",
-        SNAP_ID: "local:http://localhost:8080"
+        SNAP_ID: "npm:or-snap"
     }
 }
 
