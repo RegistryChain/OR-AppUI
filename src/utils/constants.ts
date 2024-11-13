ae1d338dec6449aba3977f9e212924bcb09051d5
Husam <712919+drhus@users.noreply.github.com>
2024-11-13 13:15:52 -0300

    Update constants.ts

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -1,10 +1,11 @@
 export const CONSTANTS = {
     AppUI_TWITTER: "https://x.com/RegistryChain",
     AppUI_DISCORD: "https://discord.gg/#",
+    AppUI_TELEGRAM: "https://t.me/onchainreputation",
     AppUI_DEMO: "#",
     AppUI_WHITELIST: "#",
     AppUI_URL: "https://or.immo/",
-    AppUI_NAME: "Onchain Registration",
+    AppUI_NAME: "Onchain Reputation",
     AppCONTRACTS_GITHUB: "https://github.com/RegistryChain/OR-contracts",
     AUTHOR_EMAIL: "h@supercontinet.com",
   };
